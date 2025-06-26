2aea535c78568add90b10a53f94fff0452e7995d
fivefootwan <wanvipa.wb@gmail.com>
2025-06-26 15:02:46 +0700

    inital set up

---
+++ my-practice/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>this is header</h1>
+      </header>
+
+      <body>
+
+
+
+      </body>
+    </div>
+  );
+}
+
+export default App;
